453f3974918c4dbe867d229e36eaadd53bbf080b
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-07-13 13:42:57 -0400

    create route for hospital

--- first_responder_api/app/controllers/hospitals_controller.rb
+++ first_responder_api/app/controllers/hospitals_controller.rb
@@ -4,4 +4,20 @@ class HospitalsController < ApplicationController
         hospitals = Hospital.all 
         render json: hospitals, include [:patients]
     end
+
+    def create 
+        hospital = Hospital.new(hospital_params)
+        if hospital.save 
+            render json: hospital
+        else 
+            render json: {
+                error: hospital.errors.full_messages
+            }
+    end
+
+    private 
+
+    def hospital_params 
+        params.require(:hospital).permit(:name, :address, :phone_number)
+    end
 end
