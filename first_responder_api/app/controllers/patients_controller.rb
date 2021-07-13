8737e4cc339d9742ef1936b6fa850d8b8b87ccbc
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-07-13 13:54:19 -0400

    added params and create route for paitent

--- first_responder_api/app/controllers/patients_controller.rb
+++ first_responder_api/app/controllers/patients_controller.rb
@@ -4,4 +4,22 @@ class PatientsController < ApplicationController
         patients = Patient.all 
         render json: patients
     end
+
+    def create 
+        patient = Patient.new(patient_params)
+            if patient.save 
+                render json: patient
+            else 
+                render json: {
+                patient.errors.full_messages
+            }
+        end 
+
+    end
+
+
+    private 
+    def patient_params 
+        params.require(:patient).permit(:first_name, :last_name, :age, :gender, :pronouns, :heart_rate, :blood_pressure, :respiration_rate, :pulse, :temperature, :blood_glucose_level, :end_tidal, :chief_complaint, :on_scene_description, :hospital_id, :id)
+    end
 end
