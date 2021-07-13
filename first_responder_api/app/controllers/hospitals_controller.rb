df2938ad5b832ffda6194a13af1a27d21e899786
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-07-13 13:38:56 -0400

    added index routes

--- first_responder_api/app/controllers/hospitals_controller.rb
+++ first_responder_api/app/controllers/hospitals_controller.rb
@@ -1,2 +1,7 @@
 class HospitalsController < ApplicationController
+
+    def index 
+        hospitals = Hospital.all 
+        render json: hospitals, include [:patients]
+    end
 end
--- first_responder_api/app/controllers/patients_controller.rb
+++ first_responder_api/app/controllers/patients_controller.rb
@@ -1,2 +1,7 @@
 class PatientsController < ApplicationController
+
+    def index 
+        patients = Patient.all 
+        render json: patients
+    end
 end
