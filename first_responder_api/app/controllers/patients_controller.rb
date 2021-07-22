3fd8cb32adb33fe8a8c48e9f5a5f9c303042b7bf
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-07-22 14:08:21 -0400

    added to patient params

--- first_responder_api/app/controllers/patients_controller.rb
+++ first_responder_api/app/controllers/patients_controller.rb
@@ -45,6 +45,6 @@ class PatientsController < ApplicationController
 
     private 
     def patient_params 
-        params.require(:patient).permit(:first_name, :last_name, :age, :gender, :pronouns, :heart_rate, :blood_pressure, :respiration_rate, :pulse, :temperature, :blood_glucose_level, :end_tidal, :chief_complaint, :on_scene_description, :hospital_id)
+        params.require(:patient).permit(:first_name, :last_name, :age, :gender, :pronouns, :heart_rate, :blood_pressure, :respiration_rate, :pulse, :temperature, :blood_glucose_level, :end_tidal, :chief_complaint, :on_scene_description, :hospital_id, :arrival_time, :date_of_arrival)
     end
 end
